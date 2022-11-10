ab4efe27c86a55843f91670f371f561622a43781
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-10 21:03:51 +0600

    edit review done

--- photography-world/src/components/Reviews/MyReview/MyReview.js
+++ photography-world/src/components/Reviews/MyReview/MyReview.js
@@ -6,10 +6,7 @@ import MyReviewRow from './MyReviewRow';
 
 const MyReview = () => {
     const { user, setLoading, loading } = useContext(AuthContext)
-    // console.log(user);
     const [reviews, setReviews] = useState();
-    console.log(reviews);
-
     // get-reviews 
     useEffect(() => {
         const url = `http://localhost:5000/user-review?id=${user?.uid}`;
@@ -23,6 +20,8 @@ const MyReview = () => {
             .catch(err => console.log(err))
 
     }, [user?.uid, setLoading])
+
+    //?delete-review
 
     const deleteReview = (id) => {
         const proceed = window.confirm('Are your sure you want to cancel this order')
@@ -43,6 +42,27 @@ const MyReview = () => {
                 .catch(err => errorToast(err))
         }
     }
+    //? edit-review 
+
+    const updateReview = (id, rate, review) => {
+        setLoading(true)
+        fetch(`http://localhost:5000/review-update/${id}`, {
+            method: "PATCH",
+            headers: {
+                'content-type': 'application/json',
+                // authorization: `Bearer ${localStorage.getItem('genius-token')}`
+            },
+            body: JSON.stringify({ rate: rate, review: review })
+        })
+            .then(res => res.json())
+            .then(data => {
+                successToast('The review updated successfully')
+                setReviews(data)
+                setLoading(false)
+            })
+            .catch(err => errorToast(err))
+    }
+
 
     return (
         <Container>
@@ -68,11 +88,11 @@ const MyReview = () => {
                                 {
                                     reviews &&
                                         reviews.length > 0 ?
-                                        reviews.map(review => <MyReviewRow key={review._id} allReviews={review} deleteReview={deleteReview}></MyReviewRow>)
+                                        reviews.map(review => <MyReviewRow key={review._id} allReviews={review} deleteReview={deleteReview} updateReview={updateReview}></MyReviewRow>)
                                         :
-                                        <tr>
-                                            <h5 className='text-center text-white py-5'>No reviews found!</h5>
-                                        </tr>
+
+                                        <h5 className='text-center text-white py-5'>No reviews found!</h5>
+
                                 }
 
                             </tbody>
--- photography-world/src/components/Reviews/MyReview/MyReviewRow.js
+++ photography-world/src/components/Reviews/MyReview/MyReviewRow.js
@@ -1,12 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Button, Spinner } from 'react-bootstrap';
-import { FaEdit, FaStar, FaTrash } from 'react-icons/fa';
+import { FaCheck, FaEdit, FaRegWindowClose, FaStar, FaTrash } from 'react-icons/fa';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
-const MyReviewRow = ({ allReviews, deleteReview }) => {
+const MyReviewRow = ({ allReviews, deleteReview, updateReview }) => {
+    const [editShow, setEditShow] = useState(false);
+
+
+
     const { setLoading, loading } = useContext(AuthContext)
     const { _id, rate, review, createdAT } = allReviews
     const [service, setService] = useState();
+    const [editRate, setEditRate] = useState();
+    const [editReview, setEditReview] = useState();
 
     // get-reviews 
     useEffect(() => {
@@ -28,13 +34,47 @@ const MyReviewRow = ({ allReviews, deleteReview }) => {
                     <Spinner animation="border" role="status" className='text-white'>
                         <span className="visually-hidden">Loading...</span>
                     </Spinner>
-                    : <tr>
+                    :
+
+                    <tr>
                         <td>{service && service.map(s => s.service_name)}</td>
-                        <td><FaStar className='text-warning mx-1' />{rate}</td>
-                        <td>{review}</td>
                         <td>{createdAT}</td>
-                        <td><Button variant='primary' className='me-2'><FaEdit></FaEdit></Button><Button onClick={() => deleteReview(_id)} variant='danger'><FaTrash></FaTrash></Button></td>
-                    </tr>}
+                        <td>
+                            {editShow ?
+                                <div className="rate">
+                                    <input type="radio" id="star5" name="rate" onChange={(e) => setEditRate(e.target.value)} value="5" />
+                                    <label htmlFor="star5" title="text">5 stars</label>
+                                    <input type="radio" id="star4" name="rate" onChange={(e) => setEditRate(e.target.value)} value="4" />
+                                    <label htmlFor="star4" title="text">4 stars</label>
+                                    <input type="radio" id="star3" name="rate" onChange={(e) => setEditRate(e.target.value)} value="3" />
+                                    <label htmlFor="star3" title="text">3 stars</label>
+                                    <input type="radio" id="star2" name="rate" onChange={(e) => setEditRate(e.target.value)} value="2" />
+                                    <label htmlFor="star2" title="text">2 stars</label>
+                                    <input type="radio" id="star1" name="rate" onChange={(e) => setEditRate(e.target.value)} value="1" />
+                                    <label htmlFor="star1" title="text">1 star</label>
+                                </div>
+                                :
+                                <><FaStar className='text-warning mx-1' />{rate}</>
+
+                            }
+                        </td>
+                        <td>{editShow ? <textarea placeholder='put your review' name="review" className="form-control" id="" cols="10" rows="1" required onChange={(e) => setEditReview(e.target.value)}></textarea> : review}</td>
+                        <td>
+
+                            {
+                                editShow ?
+                                    <>
+                                        <Button onClick={() => updateReview(_id, editRate, editReview)} variant='primary' className='me-2'><FaCheck></FaCheck> </Button>
+                                        <Button onClick={() => setEditShow(!editShow)} variant='warning' className='me-2'><FaRegWindowClose></FaRegWindowClose> </Button>
+                                    </>
+                                    :
+                                    <Button onClick={() => setEditShow(!editShow)} variant='primary' className='me-2'><FaEdit></FaEdit> </Button>
+                            }
+
+                            <Button onClick={() => deleteReview(_id)} variant='danger'><FaTrash></FaTrash></Button>
+                        </td>
+                    </tr>
+            }
         </>
     );
 };
