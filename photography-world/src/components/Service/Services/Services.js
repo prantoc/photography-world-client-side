45e4f3779eb10301573fa196143578a3a70bfa2c
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-12-21 22:53:30 +0600

    loading issue solved

--- photography-world/src/components/Service/Services/Services.js
+++ photography-world/src/components/Service/Services/Services.js
@@ -20,12 +20,16 @@ const Services = () => {
         <Container className='py-5 d-flex justify-content-center'>
             <Row>
                 <h1 className='text-center py-5' style={{ color: '#00a0ff', fontWeight: 'bold' }}>All Services</h1>
-                {loading ?
+
+                {
+                    loading &&
                     <Spinner animation="border" role="status" className='text-white'>
                         <span className="visually-hidden">Loading...</span>
                     </Spinner>
-                    :
-                    loading === false &&
+                }
+                {
+                    services.length > 0
+                    &&
                     services.map(service =>
                         <ServiceCard key={service._id} service={service}></ServiceCard>
                     )
